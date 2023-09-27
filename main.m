2c6c37975fe7be527c16d43290b1d9233bf96338
oscarboullosa <oscar.boullosa@estudiantat.upc.edu>
2023-09-27 12:07:07 +0200

    Ground Track

--- main.m
+++ main.m
@@ -1,21 +1,23 @@
+clear;
 [baseweek, esec, NS, Eph] = almanac();
 [numRows,numColumns]=size(Eph);
 G=6.67384e-11;%Gravitational Constant
 M=5.972e24;%Earth mass
-AngSpeedEarth=7.2921151467e-5;
-t=esec;
-t0=Eph(2,4);
-dt=t-t0;%Tiempo actual tuyo desde el inicio de la semana menos la semana
-sqrta=Eph(2,7);%Matriz de la raiz del semieje mayor
-a=sqrta^2;%Semieje mayor
+AngSpeedEarth=7.2921151467e-5;%Angular speed of Earth rotation
+t=esec;%Tiempo actual
+t0=Eph(2,4);%ToA del almanac
+dt=t-t0;%Diferencia de tiempo entre el ToA y el tiempo actual
+sqrt_a=Eph(2,7);%Matriz de la raiz del semieje mayor
+a=(sqrt_a)^2;%Semieje mayor
 n=sqrt((G*M)/a^3);%Mean motion
-Omega_o=Eph(2,8);
-w=Eph(2,9);
-Omega_o_Prima=Eph(2,8)-AngSpeedEarth*t0;
+Omega_o_prima=Eph(2,8);%Longitude of the ascending node at the GPS week wpoch
+w=Eph(2,9);%Argument of the perigee at ToA
+Omega_o=Omega_o_prima-AngSpeedEarth*t0;%Longitude of the ascending node at the ToA
 M_o=Eph(2,10);
 i_o=Eph(2,5);
 e=Eph(2,3);
-[x,y,z] = Kepler2ECEF(a,i_o,e,Omega_o,Omega_o_Prima,w,M_o,n,dt);
+[x,y,z,aux] = Kepler2ECEF(a,i_o,e,Omega_o,Omega_o_prima,w,M_o,n,dt);
+array=aux;
 [h,Phi,Lambda] = ECEF2LLA(x,y,z);
 Latitud=rad2deg(Phi);
 Longitud=rad2deg(Lambda);
@@ -23,3 +25,5 @@ Map=load("world_110m.txt");
 plot(Map(:,1),Map(:,2),'.');
 hold on;
 plot(Longitud,Latitud,'*');
+hold on;
+text(Longitud+2,Latitud+1,sprintf('%d',Eph(2,1)));
